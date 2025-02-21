53f9c9df62126b87a378e757db4b452bde06be13
nguyendakhuong <dakhuong281202@gmail.com>
2025-02-21 20:58:28 +0700

    khuong init

---
+++ be/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const moment = require("moment");
+const cors = require("cors");
+
+const db = require("./models");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("views"));
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+const PORT = process.env.POST || 3001;
+
+app.get("/", (req, res) => {
+  res.render("ejs/example");
+});
+
+app.listen(PORT, () => {
+  console.log("server start localhost: " + PORT);
+});
